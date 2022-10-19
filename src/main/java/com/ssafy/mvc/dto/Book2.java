d61019a0faa02b9940742f3dfb3106571cde4b9a
정연진 <y_jin99@naver.com>
2022-10-19 09:52:52 +0900

    lombok 사용

---
+++ src/main/java/com/ssafy/mvc/dto/Book2.java
@@ -0,0 +1,26 @@
+package com.ssafy.mvc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book2 {
+    private String isbn;
+    private String title;
+    private String author;
+    private int price;
+    private String description;
+    private String img;
+    
+    
+}
